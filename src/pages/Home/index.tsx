92cdf2463608dfe47eb03ea3a40da5ce878e97fa
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-07-14 01:54:29 -0300

    Adjust Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,31 @@
 import { HomeContainer } from "./styles";
 import tableHands from '../../assets/img/aerial-view-business-team.jpg'
 
-const cardProps = [
-  {
-    id: 1,
-    title: "Melhores preço",
-    photo: tableHands,
-    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit mollitia possimus odit, magnam deserunt quisquam dicta saepe omnis in hic quos alias modi, qui distinctio, vero excepturi optio voluptas tenetur.",
-  },
-  {
-    id: 2,
-    title: "Melhores preço",
-    photo: tableHands,
-    text: "Beleza",
-  },
-];
-
 export function Home(){
   return (
-  <HomeContainer>
-    <h1>Olá</h1>
-    <p>Selecione um plano para começar!</p>
-    <div>
-      {/* {
-        cardProps.map((card) => () => (
-          <div key={card.id}>
-            <h2>{card.title}</h2>
-            <img src={card.photo} alt={card.title} />
-            <p>{card.text}</p>
-          </div>
-        ))
-      } */}
-    </div>
-  </HomeContainer>
-  )
+    <HomeContainer>
+      <div>
+        <h1>Olá, Amigo</h1>
+        <p>Selecione um plano para começar!</p>
+        <img src={tableHands} alt="" />
+      </div>
+      <div>
+        <p>
+          Acreditamos que a estratégia e o empreendedorismo são as bases para o
+          crescimento e o sucesso duradouro nos negócios. Sabemos que o ambiente
+          empresarial é competitivo e em constante evolução, e é por isso que
+          valorizamos a importância de uma abordagem estratégica e do espírito
+          empreendedor para impulsionar nosso crescimento. A estratégia é o
+          alicerce de todas as grandes conquistas empresariais. Ela envolve
+          definir metas claras, identificar oportunidades, compreender o mercado
+          e desenvolver um plano de ação sólido para alcançar resultados
+          excepcionais. Na [nome da sua empresa], nossa equipe é especializada
+          em desenvolver estratégias personalizadas, adaptadas às necessidades e
+          objetivos exclusivos do seu negócio. Queremos ajudar você a navegar
+          pelas complexidades do mercado, aproveitar as tendências emergentes e
+          superar seus concorrentes.
+        </p>
+      </div>
+    </HomeContainer>
+  );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,18 @@
 import { styled } from "styled-components";
 
 export const HomeContainer = styled.main`
+  display: flex;
+  gap: 2rem;
   padding-top: 2rem;
-`
+  /* align-items: center; */
+  justify-content: center;
+  img {
+    width: 400px;
+    border-radius: 8px;
+  }
+
+  p {
+    text-align: justify;
+    padding-right: 2rem;
+  }
+`;
